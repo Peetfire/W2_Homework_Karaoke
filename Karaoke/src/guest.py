32bbab8d9b55510ed4bf58c6cfd3797f8d95271b
petermordaunt <peetfire@hotmail.com>
2021-11-26 17:31:34 +0000

    added pay entry method and tested for true and false

--- Karaoke/src/guest.py
+++ Karaoke/src/guest.py
@@ -6,3 +6,13 @@ class Guest:
 
     def get_cash(self):
         return self.cash
+
+    def reduce_cash(self, amount):
+        self.cash -= amount
+
+    def pay_entry_fee(self, amount):
+        if self.cash >= amount:
+            self.reduce_cash(amount)
+            return True
+        else:
+            return False
--- Karaoke/tests/guest_test.py
+++ Karaoke/tests/guest_test.py
@@ -15,3 +15,18 @@ class TestGuest(unittest.TestCase):
         expected = 50.00
         result = self.guest.get_cash()
         self.assertEqual(expected, result)
+
+    def test_can_pay_entry_fee(self):
+        result1 = self.guest.pay_entry_fee(15)
+        self.assertTrue(result1)
+        expected = 35
+        result2 = self.guest.get_cash()
+        self.assertEqual(expected, result2)
+
+    def test_cannot_pay_entry_fee(self):
+        result1 = self.guest.pay_entry_fee(60)
+        self.assertFalse(result1)
+        expected = 50
+        result2 = self.guest.get_cash()
+        self.assertEqual(expected, result2)
+        
