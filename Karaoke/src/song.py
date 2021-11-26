3f17583ebc559d31421044cebb44d7f23bffc560
petermordaunt <peetfire@hotmail.com>
2021-11-26 12:18:18 +0000

    Created song class and tested has attributes

---
+++ Karaoke/src/song.py
@@ -0,0 +1,6 @@
+class Song:
+
+    def __init__(self, song_name, artist, song_length):
+        self.song_name = song_name
+        self.artist = artist
+        self.song_length = song_length
---
+++ Karaoke/tests/song_test.py
@@ -0,0 +1,22 @@
+import unittest
+from src.song import Song
+
+class TestSong(unittest.TestCase):
+
+    def setUp(self):
+        self.song = Song("Dry the rain", "The Beta Band", 6.06)
+
+    def test_song_has_name(self):
+        expected = "Dry the rain"
+        result = self.song.song_name
+        self.assertEqual(expected, result)
+
+    def test_song_has_name(self):
+        expected = "The Beta Band"
+        result = self.song.artist
+        self.assertEqual(expected, result)
+
+    def test_song_has_name(self):
+        expected = 6.06
+        result = self.song.song_length
+        self.assertEqual(expected, result)
