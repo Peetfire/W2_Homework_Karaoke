797f3097bc301ad73b4562e6449b7e3874bcd7f0
petermordaunt <peetfire@hotmail.com>
2021-11-28 21:58:58 +0000

    Added Refreshment class and tested

---
+++ codeclan_caraoke/tests/refreshment_test.py
@@ -0,0 +1,21 @@
+import unittest
+from src.refreshment import Refreshment
+
+class TestRefreshment(unittest.TestCase):
+    def setUp(self):
+        self.item = Refreshment("Crisps", 1.50, 20)
+
+    def test_has_name(self):
+        expected = "Crisps"
+        result = self.item.name
+        self.assertEqual(expected, result)
+
+    def test_has_price(self):
+        expected = 1.50
+        result = self.item.price
+        self.assertEqual(expected, result)
+
+    def test_has_stock(self):
+        expected = 20
+        result = self.item.stock
+        self.assertEqual(expected, result)
